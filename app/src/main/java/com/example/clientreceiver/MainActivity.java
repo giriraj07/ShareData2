a4ca2f6fde16bc76df5bd05efafed3b0ad689c8f
Giriraj Kishor <girirajrpvv@gmail.com>
2019-07-10 12:15:59 +0530

    update client code

--- app/src/main/java/com/example/clientreceiver/MainActivity.java
+++ app/src/main/java/com/example/clientreceiver/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.clientreceiver;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.app.Activity;
@@ -8,8 +7,7 @@ import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Bundle;
-import android.support.v4.content.CursorLoader;
+
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
